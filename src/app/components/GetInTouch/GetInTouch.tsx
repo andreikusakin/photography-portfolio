95f509c38c318ac1138c39adb342c20ea710a36f
andreikusakin <andrewkusakin@me.com>
2025-01-21 16:26:44 -0500

    Added contact form

--- src/app/components/GetInTouch/GetInTouch.tsx
+++ src/app/components/GetInTouch/GetInTouch.tsx
@@ -7,6 +7,29 @@ export default function GetInTouch() {
       <h1>Get In Touch</h1>
       <div className={styles.contact}>
         <div className={styles.contactItem}>
+          <a
+            href="https://andrewkusakinphotography57.pixieset.com/contact-form/cf_EfoWk4CFGPjPNv7Jk5AReGyJpZuE"
+            className={styles.contactForm_link}
+            target="_blank"
+          >
+            <div className={styles.contactItem}>
+              <h3>Contact Form</h3>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="1em"
+                height="1em"
+              >
+                <path
+                  fill="currentColor"
+                  d="M6 6v2h8.59L5 17.59L6.41 19L16 9.41V18h2V6z"
+                ></path>
+              </svg>
+            </div>
+          </a>
+        </div>
+
+        <div>
           <h3>Email</h3>
           <a
             href="mailto:
