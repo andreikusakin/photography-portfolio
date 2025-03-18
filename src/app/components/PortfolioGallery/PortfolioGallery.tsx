ebb69870e087b6374cf3fed4519f4951e2b94bdf
andreikusakin <andrewkusakin@me.com>
2025-03-18 17:47:46 -0400

    added overflow hidden

--- src/app/components/PortfolioGallery/PortfolioGallery.tsx
+++ src/app/components/PortfolioGallery/PortfolioGallery.tsx
@@ -4,7 +4,7 @@ import EmblaCarousel from "../Carousel/EmblaCarousel";
 import PageContainer from "../PageContainer/PageContainer";
 import { MotionDiv } from "../MotionDiv/MotionDiv";
 import { Gallery, Highlight } from "@/lib/data";
-import styles from "./PortfolioGallery.module.css"
+import styles from "./PortfolioGallery.module.css";
 
 export default function PortfolioGallery({
   name,
@@ -16,37 +16,39 @@ export default function PortfolioGallery({
   highlights: Highlight[];
 }) {
   return (
-    <PageContainer>
-      <MotionDiv
-        initial={{ opacity: 0, y: 5 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
-        className={styles.title}
-      >
-        <h2>{name} Highlights</h2>
-      </MotionDiv>
-      <MotionDiv
-        initial={{ opacity: 0, y: 5 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
-      >
-        <EmblaCarousel slides={highlights} />
-      </MotionDiv>
-      <MotionDiv
-        initial={{ opacity: 0, y: 5 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
-        className={styles.title}
-      >
-        <h2>Galleries</h2>
-      </MotionDiv>
-      <MotionDiv
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5, delay: 1.5, ease: "easeInOut" }}
-      >
-        <GalleryList gallery={galleries} />
-      </MotionDiv>
-    </PageContainer>
+    <div style={{ overflow: "hidden" }}>
+      <PageContainer>
+        <MotionDiv
+          initial={{ opacity: 0, y: 5 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className={styles.title}
+        >
+          <h2>{name} Highlights</h2>
+        </MotionDiv>
+        <MotionDiv
+          initial={{ opacity: 0, y: 5 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.5 }}
+        >
+          <EmblaCarousel slides={highlights} />
+        </MotionDiv>
+        <MotionDiv
+          initial={{ opacity: 0, y: 5 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
+          className={styles.title}
+        >
+          <h2>Galleries</h2>
+        </MotionDiv>
+        <MotionDiv
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1.5, ease: "easeInOut" }}
+        >
+          <GalleryList gallery={galleries} />
+        </MotionDiv>
+      </PageContainer>
+    </div>
   );
 }
