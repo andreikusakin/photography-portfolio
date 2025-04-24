7d19637e416e09401cf5761bf978be4539d1af63
andreikusakin <andrewkusakin@me.com>
2025-04-23 23:30:54 -0400

    refactor

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -9,16 +9,7 @@ export default function Footer() {
     <div className={styles.wrapper}>
       <div className={styles.content}>
         <div className={styles.col1}>
-          <div className={styles.name}>
-            <div>Andrew Kusakin</div>
-          </div>
-          
-        </div>
-        <div className={styles.col2}>
-          <div>Wedding | Portrait | Event Photography</div>
-          <div>Based in Boston, Massachusetts</div>
-        </div>
-        <div className={styles.links}>
+          <span>Menu</span>
           <ul>
             <li>
               <Link href="/portfolio">Portfolio</Link>
@@ -36,34 +27,60 @@ export default function Footer() {
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
+          {/* <div className={styles.border}></div> */}
+        </div>
+        
+        <div className={styles.col2}>
+          <span>Portfolio</span>
           <ul>
-          {socials.map(social => (
-            <li key={social.name}>
-              <Link href={social.url} target="_blank">
-                {social.name}
-              </Link>
+            <li>
+              <Link href="/wedding">Weddings</Link>
             </li>
-          ))}
+            <li>
+              <Link href="/couples">Couples</Link>
+            </li>
+            <li>
+              <Link href="/family">Family</Link>
+            </li>
+
           </ul>
         </div>
-        <div className={styles.contacts}>
-          <div>
-            <span>Email: </span>
-            <a href="mailto:kusakinphoto@gmail.com">kusakinphoto@gmail.com</a>
-          </div>
-          <div>
-            <span>Phone: </span>
-            <a href="tel:3473135300">3473135300</a>
+
+          <div className={styles.col3}>
+            <span>Contact</span>
+            <div>
+            <div>Wedding | Family | Event Photographer</div>
+            <div>Based in Boston, Massachusetts</div></div>
+            <div>
+            <div>
+              <span>Email: </span>
+              <a href="mailto:kusakinphoto@gmail.com">kusakinphoto@gmail.com</a>
+            </div>
+            <div>
+              <span>Phone: </span>
+              <a href="tel:3473135300">3473135300</a>
+            </div></div>
+            <ul>
+              {socials.map((social) => (
+                <li key={social.name}>
+                  <Link href={social.url} target="_blank">
+                    {social.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div className={styles.copyright}>
+              © 2025 Andrew Kusakin Photography LLC.
+            </div>
           </div>
         </div>
-        <div className={styles.copyright}>© 2025 Andrew Kusakin Photography LLC.</div>
-      </div>
+   
       <div className={styles.image_container}>
         <Image
           src="/wedding/valeriejoseph/18.jpg"
           alt="wedding image"
           fill
-          quality={70}
+          quality={85}
         />
       </div>
     </div>
